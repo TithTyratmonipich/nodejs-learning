ade84d1dbe10402217f5d1dc164a523e9b856a0c
Tith Tyratmonipich <tith.tyratmonipich@gmail.com>
2023-05-08 17:12:23 +0700

    add request url, file module processes

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ var dt = require('./myfirstmodule')
 
 var http = require("http")
 
+var url = require('url');
+
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-type': 'text/html'})
     response.write("hi boys, I am at DEK CHA")
     response.write("\nGreeting here" + welcome.greeting())
     response.write("\nThe date and time are currently: " + dt.myDateTime())
+    response.write("\nrequest.url:" + request.url)
+    var q = url.parse(request.url, true).query;
+    var txt = q.year + " " + q.month;
+    response.write("\ntxt:" + txt)
     response.end();
-}).listen(8090);
+}).listen(8080);
